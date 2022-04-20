7d2a5a06be920d9e300bed125239448a948e7b32
fernandonunex <fer.bsof@outlook.com>
2022-04-20 18:21:25 -0500

    Exercise 8.4 Python Crash Course Default values

--- Chapter_8_Functions/8.3_T-Shirt.py
+++ Chapter_8_Functions/8.3_T-Shirt.py
@@ -1,9 +1,8 @@
-
-def make_shirt(size, message):
+def make_shirt(size="Large", message="I love Python"):
     """This function make a T shirt
 
     Args:
-        size (str): Small, M, L, XL
+        size (str): Small, Medium, Large, XL
         message (str): Any message to be write in the shirt
     """
     print(f"Size: {size}")
@@ -12,8 +11,15 @@ def make_shirt(size, message):
 
 def run():
     # Call function using positional arguments
-    make_shirt("M", "Positional Arguments")
-    make_shirt(size="XL", message="I'm super big")
+    # make_shirt("M", "Positional Arguments")
+
+    # Call function using positional arguments
+    # make_shirt(size="XL", message="I'm super big")
+
+    # Exercise 8.4
+    make_shirt("Large")
+    make_shirt("Medium")
+    make_shirt("XL", "Never stop learning")
 
 
 if __name__ == "__main__":
