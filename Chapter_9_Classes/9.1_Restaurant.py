9e905f677e7a96cc13b5a553b16d975315b7a2bf
fernandonunex <fer.bsof@outlook.com>
2022-04-30 02:09:03 -0500

    Exercise 9.2 Three Restaurants Python Crash Course

--- Chapter_9_Classes/9.1_Restaurant.py
+++ Chapter_9_Classes/9.1_Restaurant.py
@@ -5,24 +5,26 @@ class Restaurant():
         """Initialize restaurant_name and cuisine_type"""
         self.restaurant_name = restaurant_name
         self.cuisine_type = cuisine_type
-    
 
     def describe_restaurant(self):
         """Just print the attributes"""
         print(f"Restaurant name: {self.restaurant_name}")
-        print(f"Cuisine type: {self.cuisine_type}")
+        print(f"Cuisine type: {self.cuisine_type}\n")
 
-    
     def open_restaurant(self):
         """Prints that the restaurant is open"""
         print("The restaurant is open!!!")
 
 
 if __name__ == "__main__":
-        my_restaurant = Restaurant("El huevon", "Mexican")
+    my_restaurant = Restaurant("El huevon", "Mexican")
+    your_restaurant = Restaurant("El tacon", "Peruan")
+    her_restaurant = Restaurant("El devops", "Spanish")
 
-        print(my_restaurant.restaurant_name)
-        print(my_restaurant.cuisine_type)
+    # print(my_restaurant.restaurant_name)
+    # print(my_restaurant.cuisine_type)
 
-        my_restaurant.describe_restaurant()
-        my_restaurant.open_restaurant()
+    my_restaurant.describe_restaurant()
+    your_restaurant.describe_restaurant()
+    her_restaurant.describe_restaurant()
+
