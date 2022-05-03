5a32afcfb92886c3578b2407d5e278e3ed09858a
fernandonunex <fer.bsof@outlook.com>
2022-05-02 19:46:27 -0500

    Exercise 9.5 Login Attempts Python Crash Course

--- Chapter_9_Classes/9.3_users.py
+++ Chapter_9_Classes/9.3_users.py
@@ -7,6 +7,7 @@ class User():
     def __init__(self, first_name, last_name):
         self.first_name = first_name
         self.last_name = last_name
+        self.login_attempts = 0
 
     
     def describe_user(self):
@@ -17,14 +18,36 @@ class User():
     def greet_user(self):
         print(f"Welcome, {self.first_name}!")
 
+    def increment_login_attempts(self):
+        self.login_attempts += 1
+
+    def reset_login_attempts(self):
+        self.login_attempts = 0
+
 
 def run():
     user_1 = User("Tobias", "Smith")
     user_2 = User("Friedrich", "Lattice")
 
+    # user_1.describe_user()
+    # user_2.greet_user()
 
-    user_1.describe_user()
-    user_2.greet_user()
+    #Exercise 9.3
+    user_1.increment_login_attempts()
+    user_1.increment_login_attempts()
+    user_1.increment_login_attempts()
+    user_1.increment_login_attempts()
+    user_1.increment_login_attempts()
+    user_1.increment_login_attempts()
+    user_1.increment_login_attempts()
+    user_1.increment_login_attempts()
+    user_1.increment_login_attempts()
+    user_1.increment_login_attempts()
+    user_1.increment_login_attempts()
+    user_1.increment_login_attempts()
+    print(f"Login attempts: {user_1.login_attempts}")
+    user_1.reset_login_attempts()
+    print(f"Login attempts: {user_1.login_attempts}")
 
 if __name__ == "__main__":
         run()
