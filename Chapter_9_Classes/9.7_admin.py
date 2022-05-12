5ede7e72a288fed1448a6a1fa8b9467b0cc1cf3a
fernandonunex <fer.bsof@outlook.com>
2022-05-12 00:10:35 -0500

    Exercise 9.7 Creating an instance of a class like a atribute

--- Chapter_9_Classes/9.7_admin.py
+++ Chapter_9_Classes/9.7_admin.py
@@ -23,8 +23,15 @@ class User():
 class Admin(User):
     """A simple attempt to represent an Admin that inherits from User class"""
 
-    def __init__(self, first_name, last_name, privileges):
+    def __init__(self, first_name, last_name):
         super().__init__(first_name, last_name)
+        self.privileges = Privileges()
+
+
+class Privileges():
+    """A simple attempt to represent the privileges"""
+
+    def __init__(self, privileges=['add post', 'delete post', 'ban user']) -> None:
         self.privileges = privileges
 
     def show_privileges(self):
@@ -35,7 +42,6 @@ class Admin(User):
 
 
 if __name__ == "__main__":
-    i_am = Admin('Fernando', 'nunex', [
-                 'can add post', 'can delete post', 'can ban user', ])
+    i_am = Admin('Fernando', 'nunex')
 
-    i_am.show_privileges()
+    i_am.privileges.show_privileges()
