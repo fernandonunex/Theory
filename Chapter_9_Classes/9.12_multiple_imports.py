b612407ab1d267e8e1745fdac648b33955169740
fernandonunex <fer.bsof@outlook.com>
2022-05-19 01:35:24 -0500

    Exercise 9.12 Python Crash Course Multiple imports

---
+++ Chapter_9_Classes/9.12_multiple_imports.py
@@ -0,0 +1,6 @@
+import admin
+
+if __name__ == '__main__':
+    me = admin.Admin('Fer', 'nunx')
+
+    me.privileges.show_privileges()
--- Chapter_9_Classes/users.py
+++ Chapter_9_Classes/users.py
@@ -18,24 +18,3 @@ class User():
 
     def reset_login_attempts(self):
         self.login_attempts = 0
-
-
-class Admin(User):
-    """A simple attempt to represent an Admin that inherits from User class"""
-
-    def __init__(self, first_name, last_name):
-        super().__init__(first_name, last_name)
-        self.privileges = Privileges()
-
-
-class Privileges():
-    """A simple attempt to represent the privileges"""
-
-    def __init__(self, privileges=['add post', 'delete post', 'ban user']) -> None:
-        self.privileges = privileges
-
-    def show_privileges(self):
-        """A function that prints the privileges of a user"""
-        print("The privileges are:")
-        for privilege in self.privileges:
-            print(f"--{privilege}")
