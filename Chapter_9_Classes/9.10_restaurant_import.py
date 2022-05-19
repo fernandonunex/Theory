f9b91c0fb2cd464a9501a4518e5a71e041d1968e
fernandonunex <fer.bsof@outlook.com>
2022-05-19 01:23:08 -0500

    Exercise 9.10 Python Crash Course Import Classes

---
+++ Chapter_9_Classes/9.10_restaurant_import.py
@@ -0,0 +1,9 @@
+from restaurant import Restaurant
+
+
+
+if __name__ == '__main__':
+    my_restaurant = Restaurant('El torito', 'mexican', 15)
+
+    my_restaurant.describe_restaurant()
+    my_restaurant.open_restaurant()
