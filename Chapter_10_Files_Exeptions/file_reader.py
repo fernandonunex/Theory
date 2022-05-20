adce9434da5dd9233bb2b32b351daccff67d2173
fernandonunex <fer.bsof@outlook.com>
2022-05-20 02:18:33 -0500

    Reading a file and saving the data in a list

--- Chapter_10_Files_Exeptions/file_reader.py
+++ Chapter_10_Files_Exeptions/file_reader.py
@@ -1,8 +1,19 @@
 file_path = 'txt_files/pi_digits.txt'
-with open(file_path) as file_object:
-    #contents = file_object.read()
-    for line in file_object:
-        print(line.rstrip())
 
+# with open(file_path) as file_object:
+#     contents = file_object.read()
 
 #print(contents.rstrip()) 
+
+# with open(file_path) as file_object:
+#     #contents = file_object.read()
+#     for line in file_object:
+#         print(line.rstrip())
+
+with open(file_path) as file_object:
+    lines = file_object.readlines()
+
+for line in lines:
+    print(line.rstrip())
+
+
