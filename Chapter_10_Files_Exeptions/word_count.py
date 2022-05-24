8f8fc2fb1f34e1126ab37bc7c4b266933755431e
fernandonunex <fer.bsof@outlook.com>
2022-05-24 02:44:59 -0500

    word count function to several txt files

--- Chapter_10_Files_Exeptions/word_count.py
+++ Chapter_10_Files_Exeptions/word_count.py
@@ -1,13 +1,19 @@
-file_name = 'txt_files/alice.txt'
+def count_word(filname):
+    """Count the approximate number of words in a file."""
+    try:
+        with open(filname, encoding='utf-8') as f:
+            contents = f.read()
+    except FileNotFoundError:
+        print(f"Sorry, the file {filname} does not exist")
 
-try:
-    with open(file_name, encoding='utf-8') as f:
-        contents = f.read()
-except FileNotFoundError:
-    print(f"Sorry, the file {file_name} does not exist")
+    else:
+        words = contents.split()
+        count_words = len(words)
+        print(f"The file {filname} has about {count_words} words")
 
-else:
-    words = contents.split()
-    count_words = len(words)
-    print(f"The file {file_name} has about {count_words} words")
 
+filnames = ['txt_files/alice.txt', 'txt_files/siddhartha.txt',
+            'txt_files/moby_dick.txt', 'txt_files/little_women.txt']
+
+for filname in filnames:
+    count_word(filname)
