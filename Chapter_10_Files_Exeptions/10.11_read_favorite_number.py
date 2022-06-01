ed2aca61e5e52b0d81a3c0d826d8985bfb8b199d
fernandonunex <fer.bsof@outlook.com>
2022-06-01 02:58:46 -0500

    Exercise 10.12 try-except block to remember the favorite number

--- Chapter_10_Files_Exeptions/10.11_read_favorite_number.py
+++ Chapter_10_Files_Exeptions/10.11_read_favorite_number.py
@@ -1,7 +1,13 @@
 import json
 
-favorite_number = input("Eneter your favorite number: ")
-filename = 'fav_num.json'
+filename = 'fav_num_remembered.json'
 
-with open(filename, 'w') as f:
-    json.dump(favorite_number, f)
+try:
+    with open(filename) as f:
+        fav_numb = json.load(f)
+except FileNotFoundError:
+    favorite_number = input("Eneter your favorite number: ")
+    with open(filename, 'w') as f:
+        json.dump(favorite_number, f)
+else:
+    print(f"I know your favorite number --> {fav_numb}")
