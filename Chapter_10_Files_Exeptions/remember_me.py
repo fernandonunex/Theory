4c989ec0c84367cf9b703e051d698ed3bddd642e
fernandonunex <fer.bsof@outlook.com>
2022-06-01 01:41:51 -0500

    refactoring the file, more function and more legibility

--- Chapter_10_Files_Exeptions/remember_me.py
+++ Chapter_10_Files_Exeptions/remember_me.py
@@ -1,16 +1,35 @@
 import json
 
-filename = 'username.json'
 
-try:
-    with open(filename) as f:
-        username = json.load(f)
-except FileNotFoundError:
+def get_stored_username():
+    """Get the username stored from a json file"""
+    filename = 'username.json'
+    try:
+        with open(filename) as f:
+            username = json.load(f)
+    except FileNotFoundError:
+        return None
+    else:
+        return username
+
+
+def get_new_username():
+    """Prompt for a new username"""
+    filename = 'username.json'
     username = input('What is your name? ')
     with open(filename, 'w') as f:
         json.dump(username, f)
+
+    return username
+
+
+def greet_user():
+    """Greet the user by name"""
+    username = get_stored_username()
+    if username:
+        print(f"Welcome come back, {username}!")
+    else:
+        username = get_new_username()
         print(f"We'll remember you when you come back, {username}")
 
-else:
-    print(f"Welcome come back, {username}!")
-    
+greet_user()
