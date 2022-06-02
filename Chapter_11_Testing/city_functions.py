638077cd46b1f2e206941c577265b797afd404e8
fernandonunex <fer.bsof@outlook.com>
2022-06-02 01:38:51 -0500

    Exercise 11.2 Population, 2 unit test passed

--- Chapter_11_Testing/city_functions.py
+++ Chapter_11_Testing/city_functions.py
@@ -1,6 +1,8 @@
 
-def fortmated_city(city, country):
+def fortmated_city(city, country, population=''):
     """A function that return a formatted string like City, Country"""
-    formatted_city = f"{city}, {country}."
-
-    return formatted_city.title()
+    if population:
+        formatted_city = f"{city.capitalize()}, {country.capitalize()} - population {population}."
+    else:
+        formatted_city = f"{city.capitalize()}, {country.capitalize()}."
+    return formatted_city
--- Chapter_11_Testing/test_cities.py
+++ Chapter_11_Testing/test_cities.py
@@ -9,6 +9,11 @@ class CityTestCase(unittest.TestCase):
         """Do city-country like 'Santiago, Chile.' works?"""
         formatted_city_country = fortmated_city('santiago', 'chile')
         self.assertEqual(formatted_city_country, 'Santiago, Chile.')
+    
+    def test_city_country_population(self):
+        """Do city-country like 'Santiago, Chile - population 5000000' works?"""
+        formatted_city_country_population = fortmated_city('santiago', 'chile', 5000000)
+        self.assertEqual(formatted_city_country_population, 'Santiago, Chile - population 5000000.')
 
 if __name__ == '__main__':
     unittest.main()
